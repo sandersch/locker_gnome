4430125ba89101b39ae2ed7427de28d143b5f56b
Charlie Sanders <sanderscharlie@gmail.com>
2013-04-25 12:42:16 -0500

    Initial commit
    Result of:
    
      rails new locker_gnome --edge --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LockerGnome::Application.config.secret_token = '7595fb7fec577acd6fc2b1fe440c4d960a023ccc1c11edbbfb7894849b9e28cb16132c11f9ad86c4b795cb585210e3903a956a724a8ed0726974939b462495cb'
